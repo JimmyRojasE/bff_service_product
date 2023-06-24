5fd127a94acc4af0026ecc0dda4b142a86faf48d
JimmyRojasE <ji.rojas@duocuc.cl>
2023-06-24 17:27:11 -0400

    Commit Productos BFF

--- src/main/java/cl/musicpro/bffserviceproduct/client/ProductSvcFeingClient.java
+++ src/main/java/cl/musicpro/bffserviceproduct/client/ProductSvcFeingClient.java
@@ -2,14 +2,27 @@ package cl.musicpro.bffserviceproduct.client;
 import cl.musicpro.bffserviceproduct.model.dto.CommonResponseDTO;
 
 
-
+import cl.musicpro.bffserviceproduct.model.dto.ProductDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name="bff-products-svc",url="http://localhost:8181/products-svc")
 public interface ProductSvcFeingClient {
-    
-    @GetMapping(path = "/products",produces = MediaType.APPLICATION_JSON_VALUE)
+
+    @GetMapping(path="/products",produces =MediaType.APPLICATION_JSON_VALUE)
     public CommonResponseDTO getProducts();
+
+    @GetMapping(path = "/product/{id}", produces =MediaType.APPLICATION_JSON_VALUE )
+    public CommonResponseDTO getProduct(@PathVariable int id);
+
+    @PostMapping(path = "/product", produces = MediaType.APPLICATION_JSON_VALUE , consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommonResponseDTO> addProduct(@RequestBody ProductDTO productoDTO);
+
+    @PutMapping(path = "/product/edit", produces = MediaType.APPLICATION_JSON_VALUE , consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommonResponseDTO>EditProduct(@RequestBody ProductDTO productoDTO);
+
+    @DeleteMapping(path = "/product/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE )
+    public CommonResponseDTO deleteProduct(@PathVariable int id);
 }
--- src/main/java/cl/musicpro/bffserviceproduct/service/ProductService.java
+++ src/main/java/cl/musicpro/bffserviceproduct/service/ProductService.java
@@ -3,7 +3,9 @@ package cl.musicpro.bffserviceproduct.service;
 
 
 
+import cl.musicpro.bffserviceproduct.model.dto.ProductDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import cl.musicpro.bffserviceproduct.client.ProductSvcFeingClient;
@@ -16,10 +18,32 @@ import lombok.extern.log4j.Log4j2;
 public class ProductService {
     @Autowired
     ProductSvcFeingClient productSvcFeingClient;
-    
-    public CommonResponseDTO getProducts(){
+
+    public  CommonResponseDTO getProducts(){
+
         log.info("Request cl.musicpro.bffserviceproduct.service[getProducts]");
         log.info("[getProducts] Response: "+productSvcFeingClient.getProducts().toString());
         return productSvcFeingClient.getProducts();
+    }
+    public CommonResponseDTO getProduct(int id){
+        log.info("Request cl.musicpro.bffserviceproduct.service[getProduct]");
+        log.info("[getProduct] Response: "+productSvcFeingClient.getProducts().toString());
+        return productSvcFeingClient.getProduct(id);
+    }
+
+    public CommonResponseDTO deleteProduct(int id){
+        log.info("Request cl.musicpro.bffserviceproduct.service[deleteProduct]");
+        log.info("[deleteProduct] Response: "+productSvcFeingClient.getProducts().toString());
+        return productSvcFeingClient.deleteProduct(id);
+    }
+    public ResponseEntity<CommonResponseDTO> addProduct(ProductDTO productDTO){
+        log.info("Request cl.musicpro.bffserviceproduct.service[addProduct]");
+        log.info("[addProduct] Response: "+productSvcFeingClient.getProducts().toString());
+        return productSvcFeingClient.addProduct(productDTO);
+    }
+    public ResponseEntity<CommonResponseDTO> editProduct(ProductDTO productDTO){
+        log.info("Request cl.musicpro.bffserviceproduct.service[editProduct]");
+        log.info("[editProduct] Response: "+productSvcFeingClient.getProducts().toString());
+        return productSvcFeingClient.EditProduct(productDTO);
     }
 }
